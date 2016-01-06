460d21afc2590df0f42d536947c4e2eba1fc92a0
David Morrison <dmo_sitecore_net>
2016-01-06 15:50:53 -0500

    DMO - RC2 - rev20160106 New features Added proper adapter model interfaces Added nested models for linked and children items NuGet packaging and spec file
    --
    Added nuspec configuration file
    Updated assembly info

--- source/Sitecore.ItemAdapter/Properties/AssemblyInfo.cs
+++ source/Sitecore.ItemAdapter/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("Sitecore.ItemAdapter")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Sitecore Item Adapter")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Sitecore")]
 [assembly: AssemblyProduct("Sitecore.ItemAdapter")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyVersion("0.6.1.0")]
+[assembly: AssemblyFileVersion("0.6.1.0")]
